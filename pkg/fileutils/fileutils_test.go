37837314a8e03bf7c5401e230b4b7e9d6aed5c2d
Edoardo <canobbio.e@gmail.com>
2020-02-12 15:28:41 +0100

    pkg/fileutils: added unit tests

--- pkg/fileutils/fileutils_test.go
+++ pkg/fileutils/fileutils_test.go
@@ -1,6 +1,7 @@
 package fileutils
 
 import (
+	"os"
 	"testing"
 )
 
@@ -14,7 +15,8 @@ func TestExists(t *testing.T) {
 		want    bool
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{"should return true", args{"."}, true, false},
+		{"should return false", args{"./thisfiledoesnotexist"}, false, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -31,6 +33,16 @@ func TestExists(t *testing.T) {
 }
 
 func TestCreateIfDoesntExist(t *testing.T) {
+	emptyFunc := func() { return }
+
+	didWorkCreation := func() (bool, string) {
+		if ok, _ := Exists("./deleteme"); !ok {
+			return false, "Did not create file"
+		}
+		return true, ""
+	}
+
+	defer os.RemoveAll("./deleteme")
 	type args struct {
 		path       string
 		onCreation func()
@@ -39,13 +51,18 @@ func TestCreateIfDoesntExist(t *testing.T) {
 		name    string
 		args    args
 		wantErr bool
+		didWork func() (bool, string)
 	}{
-		// TODO: Add test cases.
+		{"should create a file", args{"./deleteme", emptyFunc}, false, didWorkCreation},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := CreateIfDoesntExist(tt.args.path, tt.args.onCreation); (err != nil) != tt.wantErr {
 				t.Errorf("CreateIfDoesntExist() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if did, msg := tt.didWork(); !did {
+				t.Errorf(msg)
 			}
 		})
 	}
